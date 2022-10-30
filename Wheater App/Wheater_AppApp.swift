fe02feb2553a5a591b2075545ae77525cc9659f8
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-10-29 22:42:35 -0500

    Initial Commit

---
+++ Wheater App/Wheater_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wheater_AppApp.swift
+//  Wheater App
+//
+//  Created by Tech Fitco on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Wheater_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
