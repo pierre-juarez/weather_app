d5ae98f0432cca3042c05d32dd9e88b273584ff1
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-10-30 02:39:19 -0500

    [add] Information view

--- Weather App/WeatherView.swift
+++ Weather App/WeatherView.swift
@@ -7,20 +7,57 @@
 
 import SwiftUI
 
+
+
 struct WeatherView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  
+  
+  var body: some View {
+    
+    ZStack{
+      
+      VStack {
+        
+        
+        Text("Weather App")
+          .font(.system(size: 25))
+          .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+          .bold()
+        
+        Spacer()
+        
+        Text("My ubication")
+          .font(.largeTitle)
+        
+        
+        Text("☀️")
+          .font(.system(size: 100))
+          .padding(EdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0))
+        
+        Text("It's sunny")
+          .font(.system(size: 30))
+        
+        Text("25°C")
+          .font(.system(size: 70))
+          .bold()
+          .padding(.bottom,50)
+        
+        
+        Spacer()
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(LinearGradient(colors: [Color("gradientStartNight"),Color("gradientEndNight")], startPoint: .topLeading, endPoint: .bottomTrailing))// 1
+      .foregroundColor(Color("textColor"))
+      
     }
+    
+    
+  }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        WeatherView()
-    }
+  static var previews: some View {
+    WeatherView()
+  }
 }
